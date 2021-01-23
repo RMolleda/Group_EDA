499f4f8b5e80b2b7d30c029cf9d96ebfc68730f5
Roberto Molleda <74590852+RMolleda@users.noreply.github.com>
2021-01-23 11:40:08 +0100

    Final version of filter function

--- group_eda/src/services/api/test1.py
+++ group_eda/src/services/api/test1.py
@@ -1,6 +1,6 @@
+import pandas as pd 
+
 def give_json(countries):
-    # Importing library
-    import pandas as pd
     # Getting url to import data
     url = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
     df = pd.read_csv(url)
@@ -9,5 +9,5 @@ def give_json(countries):
     # Changing date series to datetime fromat
     df['date'] = pd.to_datetime(df['date'])
     a = df[mask].groupby('date').mean()[['total_cases']]
-    a = a.rename(columns={"total_cases": "t_c_avg"})
+    a = a.rename(columns={"total_cases": "t_c_averages"})
     return a.to_json()
